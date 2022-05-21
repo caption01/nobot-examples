01dab3436eafe667bcce85c9c5135aec0d282f31
Nutchapon Hanouypornlert <nutcha.h@Nutchapons-MacBook-Pro.local>
2022-05-21 23:01:25 +0700

    querystring, url module

--- examples/003/parse-querystring.js
+++ examples/003/parse-querystring.js
@@ -1,1 +1,8 @@
 const querystring = require('querystring');
+
+const url = 'http://www.opencanvas.co.uk?myName=Shaun&myAge=28&comment=Yes+I+am+getting+old';
+
+const queryString = url.substring(url.indexOf('?') + 1);
+
+const parsedUrl = querystring.parse(queryString);
+
--- examples/004/url.js
+++ examples/004/url.js
@@ -1,1 +1,23 @@
 const url = require('url');
+const querystring = require('querystring');
+
+const args = process.argv.slice(2);
+
+const [urlEntered] = args;
+
+if (urlEntered === undefined) {
+  console.error('Please provide this url: https://www.google.co.uk/search?q=stranger-things&name=bom');
+  process.exit(0);
+}
+
+const {
+  protocol, slashes, host, query, href
+} = url.parse(urlEntered);
+
+const queryParse = querystring.parse(query);
+
+console.log(`Using Protocol: ${protocol}`);
+console.log(`Using slashes: ${slashes}`);
+console.log(`host: ${host}`);
+console.log('queryParse', queryParse);
+console.log(`href: ${href}`);
