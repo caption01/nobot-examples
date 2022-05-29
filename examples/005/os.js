51c0c359c7ac641d0631aae37466f598b47e9424
Nutchapon Hanouypornlert <nutcha.h@Nutchapons-MacBook-Pro.local>
2022-05-29 22:47:23 +0700

    os

--- examples/005/os.js
+++ examples/005/os.js
@@ -1,1 +1,8 @@
 const os = require('os');
+
+const homeDir = os.homedir();
+
+const cpuCore = os.cpus();
+
+const coreCount = cpuCore.length;
+const [cpuModel, ...restCore] = cpuCore;
--- examples/006/open.js
+++ examples/006/open.js
@@ -1,2 +1,26 @@
 const { platform } = require('os');
 const { exec } = require('child_process');
+
+const WINDOW = 'win32';
+
+const osPlatform = platform();
+const args = process.argv.slice(2);
+const [url] = args;
+
+if (url === undefined) {
+  console.error('Please enter a URL');
+  process.exit(0);
+}
+
+let command;
+
+if (osPlatform === WINDOW) {
+  command = `start microsoft-edge:${url}`;
+} else {
+  command = `open -a "Google Chrome" ${url}`;
+}
+
+console.log(`executing command: ${command}`);
+
+exec(command);
+// node open.js "https://www.google.com"
