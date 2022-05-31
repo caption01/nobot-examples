72a695d35ed0a233245222fca637e03a09cf00d4
Nutchapon Hanouypornlert <nutcha.h@Nutchapons-MacBook-Pro.local>
2022-05-31 23:28:17 +0700

    readline module

--- examples/010/question.js
+++ examples/010/question.js
@@ -2,3 +2,32 @@ const fs = require('fs');
 const readline = require('readline');
 const { stdin, stdout } = require('process');
 const path = require('path');
+
+const createProjectDirectory = (enteredName) => {
+  const name = enteredName.trim();
+
+  if (name === '') {
+    console.log('Cannot create a project without a name');
+    process.exit(0);
+  }
+
+  const projectPath = path.join(__dirname, name);
+
+  if (fs.existsSync(projectPath)) {
+    console.log(`${projectPath} already exists`);
+    process.exit(0);
+  }
+
+  console.log(`creating a new project called ${name}`);
+  fs.mkdirSync(name);
+};
+
+const onProjectInput = (name) => {
+  interfaceInstance.close();
+  stdin.destroy();
+  createProjectDirectory(name);
+};
+
+const interfaceInstance = readline.createInterface(stdin, stdout);
+
+interfaceInstance.question('What is the name of your project?', onProjectInput);
